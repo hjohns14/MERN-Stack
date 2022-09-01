651ff1788d22a43a0fed8465ce58cd7f9e878f29
Hunter Johns <39865626+hjohns14@users.noreply.github.com>
2022-09-01 13:58:26 -0600

    Delete create_blank.bat

--- Full Stack MERN/create_blank.bat
+++
@@ -1,6 +0,0 @@
-mkdir server && cd server && mkdir config controllers models routes && npm init -y
-npm install express mongoose cors && echo "Server folder created!" &&
-cd .. && npx create-react-app client && cd client &&
-npm install tailwindcss postcss autoprefixer axios &&
-cd .. && code .
-
