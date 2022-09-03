4ba4d418c26f9101c645c8143f17c4457b760ef5
Hunter Johns <hjohns1737@gmail.com>
2022-09-03 14:51:46 -0600

    Update PersonCard.js

--- OOP and Class Components/Class Components/Johns_Hunter_Big_Inversion/client/src/components/PersonCard.js
+++ OOP and Class Components/Class Components/Johns_Hunter_Big_Inversion/client/src/components/PersonCard.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 
 export class PersonCard extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            age: props.age
+        }
+    }
+
+    birthday(){
+        this.setState({
+            age: this.state.age +1
+        })
+    }
+
     render() {
         const {firstName, lastName, age, hairColor} = this.props
         return (
             <>
                 <h1>{firstName}, {lastName}</h1>
-                <h5>Age: {age}</h5>
+                <h5>Age: {this.state.age}</h5>
                 <h5>Hair Color: {hairColor}</h5>
+                <button onClick={this.birthday.bind(this)}>Birthday for {firstName}</button>
             </>
         )
     }
